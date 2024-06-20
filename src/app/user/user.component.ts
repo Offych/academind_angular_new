2c4266f4c4b501a3cf33c26c655bc552953d1c56
Alex <off@tut.by>
2024-06-20 15:02:11 +0300

    Adding header and user components, style its, create an array with mock data, using string interpolation to show random name of user

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { MOCK_USERS } from '../data/users';
+
+const randomIndex = Math.floor(Math.random() * MOCK_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = MOCK_USERS[randomIndex];
+}
