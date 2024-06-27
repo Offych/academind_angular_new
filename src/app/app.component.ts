c19aad1fa4a2ee240fa8f6d6c29eb0c2c54314ad
Alex <off@tut.by>
2024-06-27 16:10:28 +0300

    Migrate to modules, creating module and use it. Import components and bootstrapping app using module approach. Standalone components also can be used here, needs to be added in imports array in root module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 
 import { MOCK_USERS } from './data/users';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  //imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = MOCK_USERS;
--- src/app/service/tasks.service.ts
+++ src/app/service/tasks.service.ts
@@ -5,6 +5,13 @@ import { NewTaskData } from '../tasks/task/task.model';
   providedIn: 'root',
 })
 export class TasksService {
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
   private tasks = [
     {
       id: 't1',
@@ -43,9 +50,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
