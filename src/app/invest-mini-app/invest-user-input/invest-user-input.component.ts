aadfe7719ff9f4f7975f8fcbfa9034b151470483
Alex <off@tut.by>
2024-07-02 11:00:26 +0300

    Adding two-way bindings to form inputs for collecting values

--- src/app/invest-mini-app/invest-user-input/invest-user-input.component.ts
+++ src/app/invest-mini-app/invest-user-input/invest-user-input.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invest-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './invest-user-input.component.html',
-  styleUrl: './invest-user-input.component.css'
+  styleUrl: './invest-user-input.component.css',
 })
 export class InvestUserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedValue = '5';
+  enteredDuration = '10';
 
+  onSubmit() {
+    console.log('Submitted');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedValue);
+    console.log(this.enteredDuration);
+  }
 }
